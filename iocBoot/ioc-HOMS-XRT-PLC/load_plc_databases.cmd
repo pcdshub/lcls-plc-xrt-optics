99ac222a3aa9e68c382e5f17ff93bd699411fbb4
Zachary Lentz <zlentz@slac.stanford.edu>
2021-06-15 18:08:02 -0700

    BLD: rebuild IOC

--- iocBoot/ioc-HOMS-XRT-PLC/load_plc_databases.cmd
+++ iocBoot/ioc-HOMS-XRT-PLC/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 2701
-callbackSetQueueSize(7402)
+# Total records: 3454
+callbackSetQueueSize(8908)
 dbLoadRecords("HOMS_XRT_PLC.db", "PORT=ASYN_PLC,PREFIX=PLC:XRT:HOMS:,IOCNAME=$(IOC),IOC=$(IOC)")
