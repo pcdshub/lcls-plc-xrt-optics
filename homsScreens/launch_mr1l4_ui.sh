12b2b6654651cf4c91765d930e7001a6c200c0d9
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2021-10-06 15:51:23 -0700

    ENH: add screens to repo for release

---
+++ homsScreens/launch_mr1l4_ui.sh
@@ -0,0 +1,5 @@
+#!/bin/bash                                                                                                                        
+                                                                                                                                   
+source pcds_conda                                                                                            
+                                                                                                                                   
+typhos mr1l4_homs & 
---
+++ homsScreens/xrt.cmd
@@ -0,0 +1,2 @@
+
+edm -x -eolc -m "
