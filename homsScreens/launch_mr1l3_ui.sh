a403030447fcad7d9f8a9194f52d908f3e6ec422
Nick Waters <nrw@psdev02.slac.stanford.edu>
2021-10-08 13:57:20 -0700

    ENH: reduce defualt screen size(needs testing), more conical env source

--- homsScreens/launch_mr1l3_ui.sh
+++ homsScreens/launch_mr1l3_ui.sh
@@ -1,5 +1,5 @@
-#!/bin/bash                                                                                                                        
-                                                                                                                                   
-source pcds_conda
+#!/bin/bash
+
+source /cds/group/pcds/pyps/conda/pcds_conda
 
 `pydm -m '{"YAXIS":"MR1L3:HOMS:MMS:YUP","XAXIS":"MR1L3:HOMS:MMS:XUP", "PITCH":"MR1L3:HOMS:MMS:PITCH", "MIRROR":"XRTM1 MR1L3"}' mirrorScreen.ui &`
--- homsScreens/launch_mr2l3_ui.sh
+++ homsScreens/launch_mr2l3_ui.sh
@@ -1,5 +1,5 @@
-#!/bin/bash                                                                                                                        
-                                                                                                                                   
-source pcds_conda
-                                                                                                                                   
+#!/bin/bash
+
+source /cds/group/pcds/pyps/conda/pcds_conda
+
 typhos mr2l3_homs & 
