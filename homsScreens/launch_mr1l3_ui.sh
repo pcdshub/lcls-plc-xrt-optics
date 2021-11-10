7151e6fe38f9eedc670e6584660e07611b60bbe5
Nick Waters <nrw@mec-control.pcdsn>
2021-11-09 16:02:37 -0800

    ENH: use typhos screen for mr1l3

--- homsScreens/launch_mr1l3_ui.sh
+++ homsScreens/launch_mr1l3_ui.sh
@@ -2,4 +2,6 @@
 
 source /cds/group/pcds/pyps/conda/pcds_conda
 
-`pydm -m '{"YAXIS":"MR1L3:HOMS:MMS:YUP","XAXIS":"MR1L3:HOMS:MMS:XUP", "PITCH":"MR1L3:HOMS:MMS:PITCH", "MIRROR":"XRTM1 MR1L3"}' mirrorScreen.ui &`
+#`pydm -m '{"YAXIS":"MR1L3:HOMS:MMS:YUP","XAXIS":"MR1L3:HOMS:MMS:XUP", "PITCH":"MR1L3:HOMS:MMS:PITCH", "MIRROR":"XRTM1 MR1L3"}' mirrorScreen.ui &`
+
+typhos mr1l3_homs &
