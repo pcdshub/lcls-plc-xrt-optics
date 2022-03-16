5a4f8f801a494475bdc52aab0d34f44060494d70
Ken Lauer <klauer@slac.stanford.edu>
2022-03-16 12:17:08 -0700

    DEV: rebuild IOC with ads-ioc R0.5.0

--- iocBoot/ioc-HOMS-XRT-PLC/load_plc_databases.cmd
+++ iocBoot/ioc-HOMS-XRT-PLC/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 4106
-callbackSetQueueSize(10212)
+# Total records: 4120
+callbackSetQueueSize(10240)
 dbLoadRecords("HOMS_XRT_PLC.db", "PORT=ASYN_PLC,PREFIX=PLC:XRT:HOMS:,IOCNAME=$(IOC),IOC=$(IOC)")
